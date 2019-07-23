782b10819d3d34d5d6db94a7e5c8ff7653d19c45
Haythem Ben Drissia <BenDrissiaHaythem@gmail.com>
2019-07-23 08:30:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const name = dcument.getElementById("name");
+const address = document.getElementById("address");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const textarea = document.getElementById("textarea");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+
+form.addEventListener("submit", (e) => {
+    let messages = [];
+
+    if (name.value === "" || name.value === null) {
+        messages.push("Please Type Your Full Name!")
+    }
+
+    if (password.value.length < 8) {
+        messages.push("Password Must Be Longer Than 8 Characters")
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorElement.innerText = messages.join("");
+    }
+})
